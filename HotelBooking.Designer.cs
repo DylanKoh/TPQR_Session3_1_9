95d1a1c9b90f69c61f69a5202f98b6dffed23090
KOH WEI LONG DYLAN TP <1907446G@student.tp.edu.sg>
2020-09-01 12:34:22 +0800

    Completed Hotel Bookings

--- HotelBooking.Designer.cs
+++ HotelBooking.Designer.cs
@@ -32,7 +32,25 @@
             this.btnBack = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblHotelName = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblDelegates = new System.Windows.Forms.Label();
+            this.lblCompetitors = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label7 = new System.Windows.Forms.Label();
+            this.lblTotalValue = new System.Windows.Forms.Label();
+            this.btnBook = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
@@ -72,26 +90,201 @@
             // 
             this.panel2.BackColor = System.Drawing.Color.Maroon;
             this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel2.Location = new System.Drawing.Point(0, 489);
+            this.panel2.Location = new System.Drawing.Point(0, 681);
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(1011, 56);
             this.panel2.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(48, 156);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(146, 25);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Hotel Name: ";
+            // 
+            // lblHotelName
+            // 
+            this.lblHotelName.AutoSize = true;
+            this.lblHotelName.Location = new System.Drawing.Point(200, 156);
+            this.lblHotelName.Name = "lblHotelName";
+            this.lblHotelName.Size = new System.Drawing.Size(73, 25);
+            this.lblHotelName.TabIndex = 8;
+            this.lblHotelName.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Verdana", 14F);
+            this.label4.Location = new System.Drawing.Point(389, 103);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(179, 29);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Hotel Booking";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(63, 218);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(131, 25);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "No. of Pax: ";
+            // 
+            // lblDelegates
+            // 
+            this.lblDelegates.AutoSize = true;
+            this.lblDelegates.Location = new System.Drawing.Point(200, 218);
+            this.lblDelegates.Name = "lblDelegates";
+            this.lblDelegates.Size = new System.Drawing.Size(25, 25);
+            this.lblDelegates.TabIndex = 11;
+            this.lblDelegates.Text = "0";
+            // 
+            // lblCompetitors
+            // 
+            this.lblCompetitors.AutoSize = true;
+            this.lblCompetitors.Location = new System.Drawing.Point(200, 273);
+            this.lblCompetitors.Name = "lblCompetitors";
+            this.lblCompetitors.Size = new System.Drawing.Size(25, 25);
+            this.lblCompetitors.TabIndex = 12;
+            this.lblCompetitors.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(271, 218);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(404, 25);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Number of Delegates (Excluding Head)";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(271, 273);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(237, 25);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Number of Competitor";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 328);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(987, 224);
+            this.dataGridView1.TabIndex = 15;
+            this.dataGridView1.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellValueChanged);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Room Type";
+            this.Column1.MinimumWidth = 6;
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            this.Column1.Width = 138;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Room Rate Per Night ($)";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            this.Column2.Width = 181;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Available Number of Rooms";
+            this.Column3.MinimumWidth = 6;
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            this.Column3.Width = 230;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Rooms Required";
+            this.Column4.MinimumWidth = 6;
+            this.Column4.Name = "Column4";
+            this.Column4.Width = 187;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Sub-Total";
+            this.Column5.MinimumWidth = 6;
+            this.Column5.Name = "Column5";
+            this.Column5.ReadOnly = true;
+            this.Column5.Width = 136;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(687, 578);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(177, 25);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Total Value ($): ";
+            // 
+            // lblTotalValue
+            // 
+            this.lblTotalValue.AutoSize = true;
+            this.lblTotalValue.Location = new System.Drawing.Point(870, 578);
+            this.lblTotalValue.Name = "lblTotalValue";
+            this.lblTotalValue.Size = new System.Drawing.Size(25, 25);
+            this.lblTotalValue.TabIndex = 17;
+            this.lblTotalValue.Text = "0";
+            // 
+            // btnBook
+            // 
+            this.btnBook.Location = new System.Drawing.Point(865, 631);
+            this.btnBook.Name = "btnBook";
+            this.btnBook.Size = new System.Drawing.Size(134, 44);
+            this.btnBook.TabIndex = 18;
+            this.btnBook.Text = "Book";
+            this.btnBook.UseVisualStyleBackColor = true;
+            this.btnBook.Click += new System.EventHandler(this.btnBook_Click);
             // 
             // HotelBooking
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1011, 545);
+            this.ClientSize = new System.Drawing.Size(1011, 737);
+            this.Controls.Add(this.btnBook);
+            this.Controls.Add(this.lblTotalValue);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblCompetitors);
+            this.Controls.Add(this.lblDelegates);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblHotelName);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Margin = new System.Windows.Forms.Padding(5);
             this.Name = "HotelBooking";
             this.Text = "Hotel Booking";
             this.Load += new System.EventHandler(this.HotelBooking_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -101,5 +294,22 @@
         private System.Windows.Forms.Button btnBack;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblHotelName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblDelegates;
+        private System.Windows.Forms.Label lblCompetitors;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lblTotalValue;
+        private System.Windows.Forms.Button btnBook;
     }
 }
--- HotelBooking.cs
+++ HotelBooking.cs
@@ -14,6 +14,9 @@ namespace TPQR_Session3_1_9
     {
         string HotelName;
         User _user;
+        DateTime endDate = DateTime.Parse("30/07/2020");
+        DateTime arrival;
+        int lengthOfStay = 0;
         public HotelBooking(User user, string hotelName)
         {
             InitializeComponent();
@@ -28,7 +31,136 @@ namespace TPQR_Session3_1_9
 
         private void HotelBooking_Load(object sender, EventArgs e)
         {
+            lblHotelName.Text = HotelName;
+            LoadRooms();
+            LoadData();
+        }
 
+        private void LoadData()
+        {
+            using (var context = new Session3Entities())
+            {
+                var getArrivalDetails = (from x in context.Arrivals
+                                         where x.userIdFK == _user.userId
+                                         select x).FirstOrDefault();
+                lblDelegates.Text = getArrivalDetails.numberDelegate.ToString();
+                lblCompetitors.Text = getArrivalDetails.numberCompetitors.ToString();
+                arrival = getArrivalDetails.arrivalDate;
+                lengthOfStay = (endDate - arrival).Days;
+            }
+        }
+
+        private void LoadRooms()
+        {
+            dataGridView1.Rows.Clear();
+            var numberOfDelegates = Int32.Parse(lblDelegates.Text);
+            var numberOfCompetitors = Int32.Parse(lblCompetitors.Text);
+            using (var context = new Session3Entities())
+            {
+                var getHotelDetails = (from x in context.Hotels
+                                       where x.hotelName == HotelName
+                                       select x).FirstOrDefault();
+                if (numberOfCompetitors % 2 == 0)
+                {
+                    var singleRow = new List<string>()
+                    {
+                    "Single", getHotelDetails.singleRate.ToString(), (getHotelDetails.numSingleRoomsTotal - getHotelDetails.numSingleRoomsBooked).ToString(),
+                    numberOfDelegates.ToString(), (lengthOfStay * numberOfDelegates * getHotelDetails.singleRate).ToString()
+                    };
+                    var doubleRow = new List<string>()
+                    {
+                        "Double", getHotelDetails.doubleRate.ToString(), (getHotelDetails.numDoubleRoomsTotal - getHotelDetails.numDoubleRoomsBooked).ToString(),
+                    (numberOfCompetitors/2).ToString(), (lengthOfStay * (numberOfCompetitors/2) * getHotelDetails.doubleRate).ToString()
+                    };
+                    dataGridView1.Rows.Add(singleRow.ToArray());
+                    dataGridView1.Rows.Add(doubleRow.ToArray());
+
+                }
+                else
+                {
+                    var singleRow = new List<string>()
+                    {
+                    "Single", getHotelDetails.singleRate.ToString(), (getHotelDetails.numSingleRoomsTotal - getHotelDetails.numSingleRoomsBooked).ToString(),
+                    (numberOfDelegates + 1).ToString(), (lengthOfStay * (numberOfDelegates + 1) * getHotelDetails.singleRate).ToString()
+                    };
+                    var doubleRow = new List<string>()
+                    {
+                        "Double", getHotelDetails.doubleRate.ToString(), (getHotelDetails.numDoubleRoomsTotal - getHotelDetails.numDoubleRoomsBooked).ToString(),
+                    (numberOfCompetitors/2).ToString(), (lengthOfStay * (numberOfCompetitors/2) * getHotelDetails.doubleRate).ToString()
+                    };
+                    dataGridView1.Rows.Add(singleRow.ToArray());
+                    dataGridView1.Rows.Add(doubleRow.ToArray());
+                }
+                CalculateTotal();
+            }
+
+        }
+
+        private void CalculateTotal()
+        {
+            var totalValue = 0;
+            foreach (DataGridViewRow item in dataGridView1.Rows)
+            {
+                totalValue += Convert.ToInt32(dataGridView1.Rows[item.Index].Cells[4].Value);
+            }
+            lblTotalValue.Text = totalValue.ToString();
+        }
+
+        private void dataGridView1_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.ColumnIndex == 3)
+            {
+                dataGridView1.Rows[e.RowIndex].Cells[4].Value = Convert.ToInt32(dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Value) * Convert.ToInt32(dataGridView1.Rows[e.RowIndex].Cells[1].Value) * lengthOfStay;
+                CalculateTotal();
+            }
+        }
+
+        private void btnBook_Click(object sender, EventArgs e)
+        {
+            var totalParticipants = Int32.Parse(lblDelegates.Text) + Int32.Parse(lblCompetitors.Text);
+            var getTotalCapOfRooms = 0;
+            for (int i = 0; i < dataGridView1.RowCount - 1; i++)
+            {
+                if (i == 0)
+                {
+                    getTotalCapOfRooms += Convert.ToInt32(dataGridView1.Rows[i].Cells[3].Value);
+                }
+                else if (i == 1)
+                {
+                    getTotalCapOfRooms += Convert.ToInt32(dataGridView1.Rows[i].Cells[3].Value) * 2;
+                }
+            }
+            if (getTotalCapOfRooms < totalParticipants)
+            {
+                MessageBox.Show("There are not enough rooms booked to accomodate all participants!",
+                    "Insufficient rooms booked", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                var numberOfSingleBooked = Convert.ToInt32(dataGridView1.Rows[0].Cells[3].Value);
+                var numberOfDoubleBooked = Convert.ToInt32(dataGridView1.Rows[1].Cells[3].Value);
+                using (var context  = new Session3Entities())
+                {
+                    var getHotel = (from x in context.Hotels
+                                      where x.hotelName == HotelName
+                                      select x).FirstOrDefault();
+                    var newBooking = new Hotel_Booking()
+                    {
+                        hotelIdFK = getHotel.hotelId,
+                        userIdFK = _user.userId,
+                        numSingleRoomsRequired = numberOfSingleBooked,
+                        numDoubleRoomsRequired = numberOfDoubleBooked
+                    };
+                    context.SaveChanges();
+                    getHotel.numSingleRoomsBooked += numberOfSingleBooked;
+                    getHotel.numDoubleRoomsBooked += numberOfDoubleBooked;
+                    context.SaveChanges();
+                    MessageBox.Show("Hotel booked successfully!", "Hotel Booking",
+                        MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    Close();
+                }
+                
+            }
         }
     }
 }
