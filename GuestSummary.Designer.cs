ad5341ce014a7cce0ce81ffc49ceb14c368dd3c8
KOH WEI LONG DYLAN TP <1907446G@student.tp.edu.sg>
2020-09-01 16:09:43 +0800

    Completed Graph Summary

--- GuestSummary.Designer.cs
+++ GuestSummary.Designer.cs
@@ -28,11 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.panel1 = new System.Windows.Forms.Panel();
             this.btnBack = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cbGuests = new System.Windows.Forms.ComboBox();
             this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
@@ -72,25 +81,86 @@
             // 
             this.panel2.BackColor = System.Drawing.Color.Maroon;
             this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel2.Location = new System.Drawing.Point(0, 464);
+            this.panel2.Location = new System.Drawing.Point(0, 647);
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(1004, 56);
             this.panel2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Verdana", 14F);
+            this.label2.Location = new System.Drawing.Point(398, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(206, 29);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Guest Summary";
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(12, 199);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.StackedColumn;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series2";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(992, 442);
+            this.chart1.TabIndex = 5;
+            this.chart1.Text = "chart1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(164, 151);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(176, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Type of Guests: ";
+            // 
+            // cbGuests
+            // 
+            this.cbGuests.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbGuests.FormattingEnabled = true;
+            this.cbGuests.Items.AddRange(new object[] {
+            "No Filter",
+            "Delegates",
+            "Competitors"});
+            this.cbGuests.Location = new System.Drawing.Point(346, 148);
+            this.cbGuests.Name = "cbGuests";
+            this.cbGuests.Size = new System.Drawing.Size(289, 33);
+            this.cbGuests.TabIndex = 7;
+            this.cbGuests.SelectedIndexChanged += new System.EventHandler(this.cbGuests_SelectedIndexChanged);
             // 
             // GuestSummary
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1004, 520);
+            this.ClientSize = new System.Drawing.Size(1004, 703);
+            this.Controls.Add(this.cbGuests);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Margin = new System.Windows.Forms.Padding(5);
             this.Name = "GuestSummary";
             this.Text = "Guest Summary";
+            this.Load += new System.EventHandler(this.GuestSummary_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -100,5 +170,9 @@
         private System.Windows.Forms.Button btnBack;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cbGuests;
     }
 }
--- GuestSummary.cs
+++ GuestSummary.cs
@@ -23,5 +23,79 @@ namespace TPQR_Session3_1_9
             (new AdminMain()).ShowDialog();
             Close();
         }
+
+        private void GuestSummary_Load(object sender, EventArgs e)
+        {
+            cbGuests.SelectedIndex = 0;
+            LoadGraph(0);
+        }
+
+        private void LoadGraph(int mode)
+        {
+            chart1.Series[0].Points.Clear();
+            chart1.Series[1].Points.Clear();
+            using (var context = new Session3Entities())
+            {
+                var getDetails = (from x in context.Arrivals
+                                  select x.User.countryName).Distinct();
+                if (mode == 0)
+                {
+                    chart1.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.StackedColumn;
+                    chart1.Series[1].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.StackedColumn;
+                    foreach (var item in getDetails)
+                    {
+                        var getArrivalNumbers = (from x in context.Arrivals
+                                                 where x.User.countryName == item
+                                                 select x).FirstOrDefault();
+                        var idx = chart1.Series[0].Points.AddXY(item, getArrivalNumbers.numberDelegate + getArrivalNumbers.numberHead);
+                        var idx1 = chart1.Series[1].Points.AddXY(item, getArrivalNumbers.numberCompetitors);
+                        chart1.Series[0].LegendText = "Delegates";
+                        chart1.Series[1].LegendText = "Competitors";
+                        chart1.Series[0].Points[idx].IsValueShownAsLabel = true;
+                        chart1.Series[1].Points[idx1].IsValueShownAsLabel = true;
+                    }
+                }
+                else if (mode == 1)
+                {
+                    foreach (var item in getDetails)
+                    {
+                        var getArrivalNumbers = (from x in context.Arrivals
+                                                 where x.User.countryName == item
+                                                 select x).FirstOrDefault();
+                        var idx = chart1.Series[0].Points.AddXY(item, getArrivalNumbers.numberDelegate + getArrivalNumbers.numberHead);
+                        chart1.Series[0].LegendText = "Delegates";
+                        chart1.Series[0].Points[idx].IsValueShownAsLabel = true;
+                    }
+                }
+                else
+                {
+                    foreach (var item in getDetails)
+                    {
+                        var getArrivalNumbers = (from x in context.Arrivals
+                                                 where x.User.countryName == item
+                                                 select x).FirstOrDefault();
+                        var idx1 = chart1.Series[1].Points.AddXY(item, getArrivalNumbers.numberCompetitors);
+                        chart1.Series[1].LegendText = "Competitors";
+                        chart1.Series[1].Points[idx1].IsValueShownAsLabel = true;
+                    }
+                }
+            }
+        }
+
+        private void cbGuests_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cbGuests.SelectedItem.ToString() == "No Filter")
+            {
+                LoadGraph(0);
+            }
+            else if (cbGuests.SelectedIndex == 1)
+            {
+                LoadGraph(1);
+            }
+            else
+            {
+                LoadGraph(2);
+            }
+        }
     }
 }
