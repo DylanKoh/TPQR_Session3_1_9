889d9e71a1c63cbd34ea183ba431008a2406103a
KOH WEI LONG DYLAN TP <1907446G@student.tp.edu.sg>
2020-09-01 13:09:23 +0800

    Completed Arrival Summary

--- ArrivalSummary.Designer.cs
+++ ArrivalSummary.Designer.cs
@@ -32,7 +32,30 @@
             this.btnBack = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column10 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column11 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column12 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column13 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column14 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column15 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column16 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
@@ -72,25 +95,238 @@
             // 
             this.panel2.BackColor = System.Drawing.Color.Maroon;
             this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel2.Location = new System.Drawing.Point(0, 457);
+            this.panel2.Location = new System.Drawing.Point(0, 610);
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(1104, 56);
             this.panel2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Verdana", 14F);
+            this.label2.Location = new System.Drawing.Point(447, 117);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(212, 29);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Arrival Summary";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(507, 182);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "22 July";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(507, 387);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(85, 25);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "23 July";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column9,
+            this.Column10,
+            this.Column11,
+            this.Column12,
+            this.Column13,
+            this.Column14,
+            this.Column15,
+            this.Column16});
+            this.dataGridView1.Location = new System.Drawing.Point(23, 210);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1056, 150);
+            this.dataGridView1.TabIndex = 7;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AllowUserToAddRows = false;
+            this.dataGridView2.AllowUserToDeleteRows = false;
+            this.dataGridView2.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView2.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6,
+            this.Column7,
+            this.Column8});
+            this.dataGridView2.Location = new System.Drawing.Point(23, 415);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.ReadOnly = true;
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(1056, 150);
+            this.dataGridView2.TabIndex = 8;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "9am";
+            this.Column1.MinimumWidth = 6;
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            this.Column1.Width = 85;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "10am";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            this.Column2.Width = 98;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "11am";
+            this.Column3.MinimumWidth = 6;
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            this.Column3.Width = 98;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "12pm";
+            this.Column4.MinimumWidth = 6;
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            this.Column4.Width = 98;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "1pm";
+            this.Column5.MinimumWidth = 6;
+            this.Column5.Name = "Column5";
+            this.Column5.ReadOnly = true;
+            this.Column5.Width = 85;
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "2pm";
+            this.Column6.MinimumWidth = 6;
+            this.Column6.Name = "Column6";
+            this.Column6.ReadOnly = true;
+            this.Column6.Width = 85;
+            // 
+            // Column7
+            // 
+            this.Column7.HeaderText = "3pm";
+            this.Column7.MinimumWidth = 6;
+            this.Column7.Name = "Column7";
+            this.Column7.ReadOnly = true;
+            this.Column7.Width = 85;
+            // 
+            // Column8
+            // 
+            this.Column8.HeaderText = "4pm";
+            this.Column8.MinimumWidth = 6;
+            this.Column8.Name = "Column8";
+            this.Column8.ReadOnly = true;
+            this.Column8.Width = 85;
+            // 
+            // Column9
+            // 
+            this.Column9.HeaderText = "9am";
+            this.Column9.MinimumWidth = 6;
+            this.Column9.Name = "Column9";
+            this.Column9.ReadOnly = true;
+            this.Column9.Width = 85;
+            // 
+            // Column10
+            // 
+            this.Column10.HeaderText = "10am";
+            this.Column10.MinimumWidth = 6;
+            this.Column10.Name = "Column10";
+            this.Column10.ReadOnly = true;
+            this.Column10.Width = 98;
+            // 
+            // Column11
+            // 
+            this.Column11.HeaderText = "11am";
+            this.Column11.MinimumWidth = 6;
+            this.Column11.Name = "Column11";
+            this.Column11.ReadOnly = true;
+            this.Column11.Width = 98;
+            // 
+            // Column12
+            // 
+            this.Column12.HeaderText = "12pm";
+            this.Column12.MinimumWidth = 6;
+            this.Column12.Name = "Column12";
+            this.Column12.ReadOnly = true;
+            this.Column12.Width = 98;
+            // 
+            // Column13
+            // 
+            this.Column13.HeaderText = "1pm";
+            this.Column13.MinimumWidth = 6;
+            this.Column13.Name = "Column13";
+            this.Column13.ReadOnly = true;
+            this.Column13.Width = 85;
+            // 
+            // Column14
+            // 
+            this.Column14.HeaderText = "2pm";
+            this.Column14.MinimumWidth = 6;
+            this.Column14.Name = "Column14";
+            this.Column14.ReadOnly = true;
+            this.Column14.Width = 85;
+            // 
+            // Column15
+            // 
+            this.Column15.HeaderText = "3pm";
+            this.Column15.MinimumWidth = 6;
+            this.Column15.Name = "Column15";
+            this.Column15.ReadOnly = true;
+            this.Column15.Width = 85;
+            // 
+            // Column16
+            // 
+            this.Column16.HeaderText = "4pm";
+            this.Column16.MinimumWidth = 6;
+            this.Column16.Name = "Column16";
+            this.Column16.ReadOnly = true;
+            this.Column16.Width = 85;
             // 
             // ArrivalSummary
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1104, 513);
+            this.ClientSize = new System.Drawing.Size(1104, 666);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Margin = new System.Windows.Forms.Padding(5);
             this.Name = "ArrivalSummary";
             this.Text = "Arrival Summary";
+            this.Load += new System.EventHandler(this.ArrivalSummary_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -100,5 +336,26 @@
         private System.Windows.Forms.Button btnBack;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column9;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column10;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column11;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column12;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column13;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column14;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column15;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column16;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column8;
     }
 }
--- ArrivalSummary.cs
+++ ArrivalSummary.cs
@@ -23,5 +23,86 @@ namespace TPQR_Session3_1_9
             (new AdminMain()).ShowDialog();
             Close();
         }
+
+        private void ArrivalSummary_Load(object sender, EventArgs e)
+        {
+            dataGridView1.ColumnHeadersVisible = false;
+            dataGridView2.ColumnHeadersVisible = false;
+            LoadTimings();
+            LoadData();
+        }
+
+        private void LoadTimings()
+        {
+            dataGridView1.Rows.Clear();
+            dataGridView2.Rows.Clear();
+            var timing = new List<string>()
+            {
+                "9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm"
+            };
+            dataGridView1.Rows.Add(timing.ToArray());
+            dataGridView2.Rows.Add(timing.ToArray());
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                foreach (DataGridViewColumn cell in dataGridView1.Columns)
+                {
+                    if (dataGridView1.Rows[row.Index].Cells[cell.Index].Value.ToString() == "10am" || dataGridView1.Rows[row.Index].Cells[cell.Index].Value.ToString() == "1pm" || dataGridView1.Rows[row.Index].Cells[cell.Index].Value.ToString() == "2pm")
+                    {
+                        dataGridView1.Rows[row.Index].Cells[cell.Index].Style.BackColor = Color.Black;
+                    }
+                }
+            }
+            foreach (DataGridViewRow row in dataGridView2.Rows)
+            {
+                foreach (DataGridViewColumn cell in dataGridView2.Columns)
+                {
+                    if (dataGridView2.Rows[row.Index].Cells[cell.Index].Value.ToString() == "9am" || dataGridView2.Rows[row.Index].Cells[cell.Index].Value.ToString() == "12pm" || dataGridView2.Rows[row.Index].Cells[cell.Index].Value.ToString() == "4pm")
+                    {
+                        dataGridView2.Rows[row.Index].Cells[cell.Index].Style.BackColor = Color.Black;
+                    }
+                }
+            }
+        }
+
+        private void LoadData()
+        {
+            using (var context = new Session3Entities())
+            {
+                var date22 = DateTime.Parse("22 July");
+                var date23 = DateTime.Parse("23 July");
+                foreach (DataGridViewRow row in dataGridView1.Rows)
+                {
+                    foreach (DataGridViewColumn cell in dataGridView1.Columns)
+                    {
+                        var timing = dataGridView1.Rows[row.Index].Cells[cell.Index].Value.ToString();
+                        var getArrivalDetails = (from x in context.Arrivals
+                                                 where x.arrivalDate == date22 && x.arrivalTime == timing
+                                                 select x);
+                        var sb = new StringBuilder($"{dataGridView1.Rows[row.Index].Cells[cell.Index].Value}");
+                        foreach (var item in getArrivalDetails)
+                        {
+                            sb.Append($"\n\n{item.User.countryName}\n({item.numberCars + item.number19seat + item.number42seat}Veh)");
+                        }
+                        dataGridView1.Rows[row.Index].Cells[cell.Index].Value = sb.ToString();
+                    }
+                }
+                foreach (DataGridViewRow row in dataGridView2.Rows)
+                {
+                    foreach (DataGridViewColumn cell in dataGridView2.Columns)
+                    {
+                        var timing = dataGridView2.Rows[row.Index].Cells[cell.Index].Value.ToString();
+                        var getArrivalDetails = (from x in context.Arrivals
+                                                 where x.arrivalDate == date22 && x.arrivalTime == timing
+                                                 select x);
+                        var sb = new StringBuilder($"{dataGridView2.Rows[row.Index].Cells[cell.Index].Value}");
+                        foreach (var item in getArrivalDetails)
+                        {
+                            sb.Append($"\n\n{item.User.countryName}\n({item.numberCars + item.number19seat + item.number42seat}Veh)");
+                        }
+                        dataGridView2.Rows[row.Index].Cells[cell.Index].Value = sb.ToString();
+                    }
+                }
+            }
+        }
     }
 }
